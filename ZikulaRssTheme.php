4be84004626cd130ce9629f5ccf75095c9a267c8
Craig Heydenburg <craigh@mac.com>
2020-02-03 15:55:32 -0500

    Extensions refactor (#4103)
    Refactor modules and themes into one concept -> extensions.
    Refs #3644

---
+++ ZikulaRssTheme.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula Foundation - https://ziku.la/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zikula\RssTheme;
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Zikula\ThemeModule\AbstractCoreTheme;
+
+class ZikulaRssTheme extends AbstractCoreTheme
+{
+    /**
+     * Override parent method in order to add Content-type header to Response.
+     */
+    public function generateThemedResponse(string $realm, Response $response, string $moduleName = null): Response
+    {
+        /* @var Environment $twig */
+        $twig = $this->getContainer()->get('twig');
+
+        $output = $twig->render('@ZikulaRssTheme/master.html.twig', ['maincontent' => $response->getContent()]);
+        $newResponse = new Response($output);
+        $newResponse->headers->add(['Content-type' => 'application/rss+xml']);
+
+        return $newResponse;
+    }
+}
